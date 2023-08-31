df4ee3e79dfa2083b8018d81b6173dcf642a7def
Dylan Zhuang <dylan.zhuang@ufl.edu>
2023-08-31 11:35:21 -0400

    Second Message!

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ recruiter_name = input("Enter recruiter name: ")
 print(f"\nHi {recruiter_name},\n")
 print(f"I'm attending UF in the Class of 2026. I came across the {role} role at {company} and am interested in applying. Would you be open to sharing my resume with the hiring team so they know about my interest in this role? Happy to chat more if you have the time.\n")
 print("- Dylan")
+
+print(f"\nHi {recruiter_name},\n")
+print(f"I'm attending UF in the Class of 2026. I'm interested in exploring an internship at {company}, but I can't find any way to apply. If {company} is hiring for Summer 2024, would you be open to sharing my resume with the hiring team so they know about my interest in this role?\n")
+print("- Dylan")
