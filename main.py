2b9b37a10d036aaada026e489fee6e17eb39c865
Dylan Zhuang <dylan.zhuang@ufl.edu>
2023-08-31 11:07:58 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+company = input("Enter company name: ")
+role = input("Enter role: ")
+recruiter_name = input("Enter recruiter name: ")
+
+print(f"\nHi {recruiter_name},\n")
+print(f"I'm attending UF in the Class of 2026. I came across the {role} role at {company} and am interested in applying. Would you be open to sharing my resume with the hiring team so they know about my interest in this role? Happy to chat more if you have the time.\n")
+print("- Dylan")
